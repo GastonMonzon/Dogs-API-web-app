4588651dbfb7b78f6ae0508f8aa128e777885fe1
GastonMonzon <gastonmonzon3tonmonzon3@gmail.com>
2024-03-15 16:08:16 -0300

    Railway check

--- server/src/app.js
+++ server/src/app.js
@@ -13,14 +13,14 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'dogs-api-web-app-production.up.railway.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
-server.use('dogs-api-web-app-production.up.railway.app/', routes);
+server.use('/', routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars

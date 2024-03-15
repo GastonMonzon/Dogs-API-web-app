5cca9a7d9062cbaa9d84e8a2309af1c97daaf322
GastonMonzon <gastonmonzon3tonmonzon3@gmail.com>
2024-03-15 15:09:12 -0300

    Railway changes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+
+const port = process.env.PORT || 3000;
 
 conn.sync({ alter: true })
   .then(() => {
-    server.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
+    server.listen(port, "0.0.0.0", () => {
+      console.log(`Server listening on port ${port}`);
     })
   })
   .catch(error => console.error(error));

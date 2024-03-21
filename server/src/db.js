2032cf7f3441eacbc3e3a1efee3106e375c9e661
GastonMonzon <gastonmonzon3tonmonzon3@gmail.com>
2024-03-21 16:15:21 -0300

    Rebase Master To Local Use

--- server/src/db.js
+++ server/src/db.js
@@ -2,13 +2,9 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, API_KEY, DB_DEPLOY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_HOST}/dogs`, {
-//   logging: false,
-//   native: false,
-// });
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_HOST}/${DB_NAME}`, {
   logging: false,
   native: false,
 });
